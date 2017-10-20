43dc748b30bf524c046a9a8fb7df3af8c051ca2a
aaron <yalong1976@gmail.com>
2017-10-20 16:02:37 +0800

    add coinx exchange

---
+++ exchanges/coinx.js
@@ -0,0 +1,229 @@
+var CoinX = require('./coinxadapter.js');
+var util = require('../core/util.js');
+var _ = require('lodash');
+var moment = require('moment');
+var log = require('../core/log');
+
+var Trader = function(config) {
+  _.bindAll(this);
+  if(_.isObject(config)) {
+    this.key = config.key;
+    this.secret = config.secret;
+    this.clientID = config.username;
+    this.asset = config.asset.toLowerCase();
+    this.currency = config.currency.toLowerCase();
+    this.market = this.asset + this.currency;
+  }
+    this.name = 'coinx';
+    this.coinx = new CoinX(this.key, this.secret);
+    this.lastTid = false;
+}
+
+// if the exchange errors we try the same call again after
+// waiting 10 seconds
+Trader.prototype.retry = function(method, args) {
+    var wait = +moment.duration(10, 'seconds');
+    log.debug(this.name, 'returned an error, retrying..');
+
+    var self = this;
+
+    // make sure the callback (and any other fn)
+    // is bound to Trader
+    _.each(args, function(arg, i) {
+        if (_.isFunction(arg))
+            args[i] = _.bind(arg, self);
+    });
+
+    // run the failed method again with the same
+    // arguments after wait
+    setTimeout(
+        function() {
+            method.apply(self, args)
+        },
+        wait
+    );
+}
+
+Trader.prototype.getPortfolio = function(callback) {
+  var calculate = function(err, data) {
+    if(err) {
+      if(err.message === 'invalid api key')
+        util.die('Your ' + this.name + ' API keys are invalid');
+      return this.retry(this.coinx.getMember, calculate);
+    }
+
+    var portfolio = [];
+    _.each(data.accounts, function(account) {
+      portfolio.push({name: account.currency.toUpperCase(), amount: + account.balance});
+    });
+
+    callback(err, portfolio);
+  }.bind(this);
+
+  this.coinx.getMember(calculate);
+}
+
+Trader.prototype.getTicker = function(callback) {
+  this.coinx.ticker(this.asset, this.currency, callback);
+}
+
+Trader.prototype.getFee = function(callback) {
+  var makerFee = 0.1;
+  callback(false, makerFee / 100);
+}
+
+Trader.prototype.buy = function(amount, price, callback) {
+  var args = _.toArray(arguments);
+  var set = function(err, result) {
+    if(err || result.status === "error") {
+      log.error('unable to buy:', err, result.reason, 'retrying...');
+      return this.retry(this.buy, args);
+    }
+
+    callback(null, result.id);
+  }.bind(this);
+
+  //Decrease amount by 1% to avoid trying to buy more than balance allows.
+  amount -= amount / 100;
+
+  amount *= 100000000;
+  amount = Math.floor(amount);
+  amount /= 100000000;
+
+  // prevent:
+  // 'Ensure that there are no more than 2 decimal places.'
+  // price *= 100;
+  // price = Math.floor(price);
+  // price /= 100;
+
+  this.coinx.buy(this.asset, this.currency, price, amount, set);
+}
+
+Trader.prototype.sell = function(amount, price, callback) {
+  var args = _.toArray(arguments);
+  var set = function(err, result) {
+    if(err || result.status === "error") {
+      log.error('unable to sell:', err, result.reason, 'retrying...');
+      return this.retry(this.sell, args);
+    }
+
+    callback(null, result.id);
+  }.bind(this);
+
+  // prevent:
+  // 'Ensure that there are no more than 8 decimal places.'
+  amount *= 100000000;
+  amount = Math.floor(amount);
+  amount /= 100000000;
+
+  // prevent:
+  // 'Ensure that there are no more than 2 decimal places.'
+  // price *= 100;
+  // price = Math.ceil(price);
+  // price /= 100;
+
+  this.coinx.sell(this.asset, this.currency, price, amount, set);
+}
+
+
+Trader.prototype.getOrder = function(order_id, callback) {
+  var args = _.toArray(arguments);
+  var get = function(err, data) {
+    if(!err && _.isEmpty(data) && _.isEmpty(data.result))
+      err = 'no data';
+
+    else if(!err && !_.isEmpty(data.error))
+      err = data.error;
+
+    if(err) {
+      log.error('Unable to get order', order, JSON.stringify(err));
+      return this.retry(this.getOrder, args);
+    }
+
+    var order = _.find(data, o => o.uid === +order_id);
+
+    if(!order) {
+      // if the order was cancelled we are unable
+      // to retrieve it, assume that this is what
+      // is happening.
+      return callback(err, {
+        price: 0,
+        amount: 0,
+        date: moment(0)
+      });
+    }
+
+    var price = parseFloat( order.price );
+    var amount = Math.abs(parseFloat( order.volume ));
+    var date = moment( order.created_at );
+
+    callback(err, {price, amount, date});
+  }.bind(this);
+
+  this.coinx.getOrder(order_id, get);
+}
+
+Trader.prototype.checkOrder = function(order_id, callback) {
+  var check = function(err, result) {
+    var stillThere = _.find(result, function(o) { return o.id === order });
+    callback(err, !stillThere);
+  }.bind(this);
+
+  this.coinx.getOrder(order_id, check);
+}
+
+Trader.prototype.cancelOrder = function(order_id, callback) {
+  var args = _.toArray(arguments);
+  var cancel = function(err, result) {
+    if(err || !result) {
+      log.error('unable to cancel order', order, '(', err, result, ')');
+      return this.retry(this.cancelOrder, args);
+    }
+
+    callback();
+  }.bind(this);
+
+  this.coinx.cancel_order(order_id, cancel);
+}
+
+Trader.prototype.getTrades = function(since, callback, descending) {
+  var args = _.toArray(arguments);
+  var process = function(err, trades) {
+    if(err)
+      return this.retry(this.getMyTrades, args);
+
+    var result = _.map(trades, t => {
+      return {
+        date: t.created_at,
+        tid: +t.id,
+        price: +t.price,
+        amount: +t.origin_volume
+      }
+    })
+
+    callback(null, result.reverse());
+  }.bind(this);
+
+  this.coinx.getMyTrades(this.asset, this.currency, null, null, null, null, null, process);
+}
+
+Trader.getCapabilities = function () {
+  return {
+    name: 'CoinX',
+    slug: 'coinx',
+    currencies: ['BTC', 'ETH'],
+    assets: ['ETH', 'OPC'],
+    maxTradesAge: 60,
+    markets: [
+      { pair: ['BTC', 'ETH'], minimalOrder: { amount: 0.001, unit: 'currency' } },
+      { pair: ['BTC', 'OPC'], minimalOrder: { amount: 5, unit: 'asset' } },
+      { pair: ['ETH', 'OPC'], minimalOrder: { amount: 5, unit: 'asset' } },
+    ],
+    requires: ['key', 'secret', 'username'],
+    fetchTimespan: 60,
+    tid: 'id',
+    tradable: true
+  };
+}
+
+module.exports = Trader;
---
+++ exchanges/coinxadapter.js
@@ -0,0 +1,417 @@
+//Create module for CoinX exchange to call the APIs
+module.exports = (function() {
+    'use strict';
+
+    // Module dependencies
+    var crypto  = require('crypto'),
+        request = require('request'),
+        nonce   = require('nonce')();
+    var Gkey=''
+    var Gsign='' //global var, FIXME should use one from constructor
+    var GSecret = '';
+
+    // Constants
+    var version         = '0.0.6',
+        API_URL  = 'https://x.tth365.com:443//api/v2',
+        HASH_URL = '/api/v2',
+        USER_AGENT      = 'coinx.js ' + version;
+    var errorMsg = "Missing Params";
+    //USER_AGENT    = 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0'
+
+
+    // Helper methods
+    function joinCurrencies(currencyA, currencyB){
+        // If only one arg, then return the first
+        if (typeof currencyB !== 'string'){
+            return currencyA;
+        }
+
+        return currencyA.toLowerCase()  + currencyB.toLowerCase();
+    }
+
+    function sortParameters(a, b){return 0;
+        // Sort `nonce` parameter last, and the rest alphabetically
+        return a === 'nonce' || a > b ? 1 : -1;
+    }
+
+    function cleanUpParam(param){
+        for (var key in param){
+            if(param[key] == null)
+                delete param[key];
+        }
+    }
+
+
+    // Constructor
+    function CoinX(key, secret){
+        Gkey = key;
+        GSecret = secret;
+        // Generate headers signed by this user's key and secret.
+        // The secret is encapsulated and never exposed
+        this._getPrivateHeaders = function(parameters,link){
+            var paramString, signature;
+            var url = "POST|"+HASH_URL+link+"|"
+            if (!key || !secret){
+                throw 'CoinX: Error. API key and secret required';
+            }
+
+            // Sort parameters alphabetically and convert to `arg1=foo&arg2=bar`
+            paramString = Object.keys(parameters).sort(parameters).map(function(param){
+                return encodeURIComponent(param) + '=' + encodeURIComponent(parameters[param]);
+            }).join('&');
+            var signature = crypto.createHmac('SHA256', secret).update(url+paramString).digest('hex');
+            parameters.signature = signature
+
+            signature = crypto.createHmac('sha256', secret).update(paramString).digest('hex');
+           //console.log("SIGNATURE " +signature)
+
+            Gkey = key
+            Gsign = signature
+            GSecret = secret
+            return {
+                Key: key,
+                Sign: signature
+            };
+        };
+    }
+
+    // Currently, this fails with `Error: CERT_UNTRUSTED`
+    // CoinX.STRICT_SSL can be set to `false` to avoid this. Use with caution.
+    // Will be removed in future, once this is resolved.
+    //CoinX.STRICT_SSL = false;
+
+    // Customisable user agent string
+    CoinX.USER_AGENT = USER_AGENT;
+
+    // Prototype
+    CoinX.prototype = {
+        constructor: CoinX,
+
+        // Make an API request
+        _request: function(options, callback){
+            request(options, function(err, response, body) {
+                // Empty response
+                if (!err && (typeof body === 'undefined' || body === null)){
+                    err = 'Empty response';
+                }
+
+                callback(err, body);
+            });
+
+            return this;
+        },
+
+        // Make a public API request
+        _public: function(link, parameters, callback){
+            var options;
+
+            if (typeof parameters === 'function'){
+                callback = parameters;
+                parameters = {};
+            }
+
+            parameters || (parameters = {});
+            options = {
+                method: 'GET',
+                url: API_URL+link,
+                qs: parameters
+            };
+            return this._request(options, callback);
+        },
+
+        // Make a private API request POST
+        _privatePost: function(link, parameters, callback){
+            var options;
+
+            if (typeof parameters === 'function'){
+                callback = parameters;
+                parameters = {};
+            }
+
+            parameters || (parameters = {});
+            parameters.tonce = nonce()/100;
+            parameters.access_key = Gkey
+            var paramString = Object.keys(parameters).sort(parameters).map(function(param){
+                return encodeURIComponent(param) + '=' + encodeURIComponent(parameters[param]);
+            }).join('&');
+
+
+           // var signature = (crypto.createHmac('sha256', secret).update(url+paramString).digest('hex')).toString();
+           // parameters.signature = signature
+           //// console.log(signature + " tonce " + parameters.tonce)
+
+
+            options = {
+                method: "POST",
+                url: API_URL+link,
+                form: parameters,
+                headers: this._getPrivateHeaders(parameters,link)
+            };
+            options.headers['User-Agent'] = "CoinX API Client/0.0.1"
+            return this._request(options, callback);
+        },
+/////
+        // Make a private API request GET
+        _privateGet: function(link,callback){
+            var options;
+            var url = "GET|"+HASH_URL+link+"|"
+
+           var parameters= {}
+            parameters.access_key = Gkey
+            parameters.tonce = parseInt(nonce()/100 );
+
+           var  paramString = Object.keys(parameters).sort(parameters).map(function(param){
+                   return encodeURIComponent(param) + '=' + encodeURIComponent(parameters[param]);
+               }).join('&');
+
+            var signature = crypto.createHmac('sha256', GSecret).update(url+paramString).digest('hex');
+            parameters.signature=signature
+            paramString = paramString +"&"+encodeURIComponent('signature') + "=" + encodeURIComponent(signature)
+            options = {
+                method: 'GET',
+                url: API_URL+link+"?"+paramString
+            };
+            return this._request(options, callback);
+        },
+
+
+        /////
+
+
+        // PUBLIC METHODS
+
+        getTicker: function(A,B,callback){
+            if(!A  || !B)
+              return  callback (errorMsg, null);
+            else {
+                var parameters = {
+                    market: joinCurrencies(A, B)
+                };
+                var url = '/tickers/' + joinCurrencies(A, B) + '.json'
+
+                return this._public(url, parameters, callback);
+            }
+        },
+        getAllTickers : function(callback){
+            return this._public("/tickers.json",{},callback);
+        },
+        getDepth: function(currencyA,currencyB,mLimit,callback) {
+            if (!currencyA || !currencyB)
+               return callback(errorMsg, null);
+            else {
+                var parameters = {
+                    market: joinCurrencies(currencyA, currencyB),
+                    limit: mLimit ? mLimit : 300
+                };
+
+            return this._public('/depth.json', parameters, callback);
+            }
+        },
+        getOrderBook: function(currencyA, currencyB,askLimit, bidLimit, callback){
+            if(!currencyA  || !currencyB)
+               return callback(errorMsg,null);
+            else {
+                var parameters = {
+                    market: joinCurrencies(currencyA, currencyB),
+                    asks_limit: askLimit ? askLimit : 20,
+                    bids_limit: bidLimit ? bidLimit : 20
+                };
+
+                return this._public('/order_book.json', parameters, callback);
+            }
+        },
+        getMarkets : function(callback){
+            return this._public("/markets.json",{},callback);
+        },
+        getTimeStamp : function(callback){
+            return this._public("/timestamp.json",{},callback);
+        },
+        getK : function(currencyA,currencyB,Limit, Period,Timestamp,callback){
+            if(!currencyA || !currencyB)
+               return callback(errorMsg,null);
+            else {
+                var param = {
+                    market: joinCurrencies(currencyA, currencyB),
+                    limit: Limit ? Limit : null,
+                    period: Period ? Period : null,
+                    timestamp: Timestamp ? Timestamp : null
+                }
+                cleanUpParam(param);
+                return this._public("/k.json", param, callback);
+            }
+        },
+        KPendingTrades : function (currencyA,currencyB, tradeId, Limit,Period,Timestamp,callback){
+            if(!currencyA || !currencyB || !tradeId)
+             return   callback(errorMsg,null);
+            else {
+                var param = {
+                    market: joinCurrencies(currencyA, currencyB),
+                    trade_id: tradeId,
+                    period: Period ? Period : null,
+                    timestamp: Timestamp ? Timestamp : null
+                }
+                cleanUpParam(param);
+                return this._public("/k_with_pending_trades.json", param, callback);
+            }
+        },
+
+
+
+        //// PRIVATE GET
+
+
+        getMember : function(callback){
+            var url = "/members/me.json";
+            return this._privateGet(url,{},callback);
+        },
+         getAllDeposits : function(InCurrency,InLimit, InState,callback){
+             if(!InCurrency)
+               return callback(errorMsg,null)
+             else {
+                 var param = {
+                     currency: InCurrency,
+                     limit: InLimit ? InLimit : 100,
+                     state: InState ? InState : "wait"
+                 }
+                 return this._privateGet("/deposits.json", param, callback);
+             }
+        },
+        getDeposit : function(transaction, callback){
+            if(!transaction)
+               return callback(errorMsg,null)
+            else {
+                var param = {
+                    txid: transaction
+                }
+                return this._privateGet("/deposit.json", param, callback);
+            }
+        },
+        getDepositAddress : function(InCurrency, callback){
+            if(!InCurrency){
+               return callback(errorMsg,null)
+            }
+            else {
+                var param = {
+                    currency: InCurrency
+                }
+                return this._privateGet("/deposit_address.json", param, callback);
+            }
+        },
+        getAllOrders : function(currencyA, currencyB, mState, mLimit, mPage, mOrderBy, callback){
+            if(!currencyA || !currencyB ){
+               return callback(errorMsg,null);
+            }
+            var param = {
+                state : mState ? mState : "wait",
+                limit : mLimit ? mLimit : 100,
+                page : mPage ? mPage  : 0 ,
+                order_by : mOrderBy? mOrderBy : 'asc',
+                market : joinCurrencies(currencyA,currencyB)
+            }
+
+            return this._privateGet("/orders.json",param,callback);
+
+        },
+        getOrder : function(orderId,callback){
+            if(!orderId)
+               return callback(errorMsg,null);
+            var param = { id : orderId}
+            return this._privateGet("/orders.json",param, callback);
+        },
+
+        myBalances: function(callback){
+            return this._privateGet('/members/me.json',callback);
+        },
+
+        getRecentTrade : function(currencyA, currencyB, mLimit, Timestamp, From, To, Orderby,callback){
+            if(!currencyA || !currencyB){
+              return  callback(errorMsg,null);
+            }
+            var param = {
+                market : joinCurrencies(currencyA,currencyB),
+                limit : mLimit ? mLimit : 50,
+                timestamp : Timestamp ? Timestamp : null,
+                from : From ? From : null,
+                to : To ? To : null,
+                order_by : Orderby  ? Orderby : "desc"
+            }
+            cleanUpParam(param);
+            return this._privateGet("/trades.json",param,callback);
+        },
+        getMyTrades :  function(currencyA, currencyB, mLimit, Timestamp, From, To, Orderby,callback){
+            if(!currencyA || !currencyB){
+               return callback(errorMsg,null);
+            }
+            var param = {
+                market : joinCurrencies(currencyA,currencyB),
+                limit : mLimit ? mLimit : 50,
+                timestamp : Timestamp ? Timestamp : null,
+                from : From ? From : null,
+                to : To ? To : null,
+                order_by : Orderby  ? Orderby : "desc"
+            }
+            cleanUpParam(param)
+            return this._privateGet("/trades/my.json",param,callback);
+        },
+
+
+        /////PRIVATE POST
+
+        buy: function(currencyA, currencyB, rate, amount, callback){
+            if(!currencyA || !currencyB || !rate || !amount){
+               return callback(errorMsg,null);
+            }
+            var parameters = {
+                market: joinCurrencies(currencyA, currencyB),
+                price: rate,
+                volume: amount,
+                side : 'buy'
+            };
+
+            return this._privatePost('/orders.json', parameters, callback);
+        },
+
+        sell: function(currencyA, currencyB, rate, amount, callback){
+            if(!currencyA || !currencyB || !rate || !amount){
+               return callback(errorMsg,null);
+            }
+            else {
+                var parameters = {
+                    market: joinCurrencies(currencyA, currencyB),
+                    price: rate,
+                    volume: amount,
+                    side: 'sell'
+                };
+
+                return this._privatePost('/orders.json', parameters, callback);
+            }
+        },
+
+        cancelAllOrders : function(mSide, callback){
+
+            if(mSide){
+                var param = {
+                    side : mSide
+                }
+                return this._privatePost("orders/clear.json",param,callback);
+            }
+            else{
+                return this._privatePost("orders/clear.json",{},callback);
+            }
+        },
+
+        cancelOrder : function(orderId,callback){
+            if(!orderId){
+                return callback(errorMsg,null);
+            }
+            else {
+                var param = {id: orderId}
+                return this._privatePost("/order/delete.json", param, callback);
+            }
+        }
+
+
+    };
+
+    return CoinX;
+})();
